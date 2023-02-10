b80c39fd4a789ed9eda74b19580fe4f658f3fc29
Ephraimiyanda <iyandaephraim@gmail.com>
2023-02-09 15:37:47 -0900

    challenge-07 done

---
+++ challenge-07/index.js
@@ -0,0 +1,10 @@
+const classroom= {
+    hasTeachingAssistant: true,
+classList:["rashida","james","roman","lisa","omair","lukas"]
+}
+function getStudents(classroom){
+let teacher= classroom[0];
+if(hasTeachingAssistant= true){
+
+}
+}
